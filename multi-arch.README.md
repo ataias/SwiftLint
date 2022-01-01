11c28459274682cc8120d43850dced16958b4251
Ataias Pereira Reis <ataiasreis@gmail.com>
2022-01-01 13:10:47 -0300

    Add latest tag

--- multi-arch.README.md
+++ multi-arch.README.md
@@ -3,7 +3,13 @@
 This fork of Swiftlint contains a [build script](./script/build-multi-arch-docker-image.sh) that helps building a
 multi-arch docker image for Swiftlint. The image supports arm64 and amd64 and it uses a [multi-arch Swift image](https://hub.docker.com/repository/docker/ataias/swift) as well.
 
-- Docker Hub: [https://hub.docker.com/repository/docker/ataias/swiftlint]
+- Docker Hub: [https://hub.docker.com/repository/docker/ataias/swiftlint](https://hub.docker.com/repository/docker/ataias/swiftlint)
 - Git Repos:
   - [ataias/SwiftLint](https://github.com/ataias/SwiftLint) (fork)
   - [gitlab.com/ataias/swift-docker-multi-arch](https://gitlab.com/ataias/swift-docker-multiarch)
+
+## Usage
+
+```sh
+docker run --volume /full/path/to/folder:/project --workdir /project ataias/swiftlint swiftlint lint
+```
--- script/build-multi-arch-docker-image.sh
+++ script/build-multi-arch-docker-image.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 SWIFT_VERSION=${SWIFT_VERSION:-5.5.2}
-SWIFTLINT_VERSION=${SWIFTLINT_VERSION:-$(git describe)}
+SWIFTLINT_VERSION=${SWIFTLINT_VERSION:-$(git describe --abbrev=0)}
 BASE_DEV_IMAGE=${BASE_DEV_IMAGE:-ataias/swift:${SWIFT_VERSION}-focal}
 BASE_PROD_IMAGE=${BASE_PROD_IMAGE:-ataias/swift:${SWIFT_VERSION}-focal}
-PROD_IMAGE=${PROD_IMAGE:-ataias/swiftlint:${SWIFTLINT_VERSION}}
+PROD_IMAGE_PREFIX=${PROD_IMAGE_PREFIX:-ataias}
+PROD_IMAGE=$PROD_IMAGE_PREFIX:${SWIFTLINT_VERSION}
+PROD_IMAGE_LATEST=$PROD_IMAGE_PREFIX:latest
 HOST_ARM64=${HOST_ARM64:-unix:///var/run/docker.sock}
 HOST_AMD64=${HOST_AMD64:-unix:///var/run/docker.sock}
 
@@ -35,3 +37,9 @@ docker manifest create -a $PROD_IMAGE \
     $PROD_IMAGE-amd64
 
 docker manifest push $PROD_IMAGE
+
+docker manifest create -a $PROD_IMAGE_LATEST \
+    $PROD_IMAGE-arm64 \
+    $PROD_IMAGE-amd64
+
+docker manifest push $PROD_IMAGE_LATEST
